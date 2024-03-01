7f7ffedd05d0b1004abdea8417d5d9f8a7dbddf2
manu.mnzz <manbuzmun@alum.us.es>
2024-03-01 10:08:44 +0100

    new file:   DeliverUS-Backend/src/controllers/OrderControllers.js 	modified:   DeliverUS-Backend/src/controllers/RestaurantController.js 	modified:   DeliverUS-Backend/src/routes/RestaurantRoutes.js 	modified:   package-lock.json

--- DeliverUS-Backend/src/controllers/RestaurantController.js
+++ DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -22,19 +22,97 @@ const index = async function (req, res) {
 // TODO: Complete the following functions
 
 const create = async function (req, res) {
+  const newRestaurant = Restaurant.build(req.body)
 
+  newRestaurant.userId = req.user.id
+
+  if (typeof req.files?.heroImage !== 'undefined') {
+    newRestaurant.heroImage = req.files.heroImage[0].destination
+  }
+
+  if (typeof req.files?.heroImage !== 'undefined') {
+    newRestaurant.logo = req.files.logo[0].destination + '/' + req.files.heroImage[0].filename
+  }
+
+  try {
+    const restaurant = await newRestaurant.save()
+
+    res.json(restaurant)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 }
 
 const show = async function (req, res) {
+  try {
+    const restaurants = await Restaurant.findAll(
+      {
+        attributes: ['id', 'name', 'description', 'address', 'postalCode', 'url', 'shippingCosts', 'averageServiceMinutes', 'email', 'phone',
+          'logo', 'heroImage', 'status', 'restaurantCategoryId'],
+        include:
+        {
+          model: RestaurantCategory, as: 'restaurantCategory'
+        },
+        order: [[{ model: RestaurantCategory, as: 'restaurantCategory' }, 'name', 'ASC']]
+      }
+    )
+    const restaurant = await Restaurant.findByPK(req.params.restaurantId, {
+      atributes: { exclude: ['userId'] },
+      include: [{
+        model: Product,
+        as: 'products',
+        include: { model: ProductCategory, as: 'productCategory' }
+      },
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      }],
+      order: [[{ model: Product, as: 'products' }, 'order', 'ASC']]
 
+    })
+    res.json(restaurant)
+    res.json(restaurants)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 }
 
 const update = async function (req, res) {
+  if (typeof req.files?.heroImage !== 'undefined') {
+    req.body.heroImage = req.files.heroImage[0].destination + '/' + req.files.heroImage[0].filename
+  }
 
+  if (typeof req.files?.logo !== 'undefined') {
+    req.body.logo = req.files.logo[0].destination + '/' + req.logo[0].filename
+  }
+
+  try {
+    await Restaurant.update(req.body, { where: { id: req.params.restaurantId } })
+
+    const updatedRestaurant = await Restaurant.findByPK(req.params.restaurantId)
+
+    res.json(updatedRestaurant)
+  } catch (error) {
+    res.catch(500).send(error)
+  }
 }
 
 const destroy = async function (req, res) {
+  try {
+    const result = await Restaurant.destroy({ where: { id: req.params.restaurantId } })
 
+    let message = ' '
+
+    if (result === 1) {
+      message = 'Id de restaurante eliminado correctamente.' + req.params.restaurantId
+    } else {
+      message = 'El Id de restaurante no ha podido ser eliminado.'
+    }
+
+    res.json(message)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 }
 
 const RestaurantController = {
--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -1,7 +1,29 @@
 import RestaurantController from '../controllers/RestaurantController.js'
 
 const loadFileRoutes = function (app) {
-  // TODO: Include routes for restaurant described in the lab session README
+  app.route('/restaurants') // the endpoint path
+    .get(
+      RestaurantController.index) // the function that will attend requests for that http verb and that path
+    .post( // we can chain more http verbs for the same endpoint
+      RestaurantController.create) // the function that will attend requests for that http verb and that path
 
+  app.route('/restaurants/:restaurantId') // the endpoint path
+    .get( // the http verb that we want to be available at the previous path
+      RestaurantController.show) // the function that will attend requests for that http verb and that path
+    .put( // we can chain more http verbs for the same endpoint
+      RestaurantController.update) // the function that will attend requests for that http verb and that path
+    .delete(RestaurantController.destroy)
+
+  app.route('/restaurants/:restaurantId/orders') // the endpoint path
+    .get( // the http verb that we want to be available at the previous path
+      OrderController.index) // the function that will attend requests for that http verb and that path
+
+  app.route('/restaurants/:restaurantId/products') // the endpoint path
+    .get( // the http verb that we want to be available at the previous path
+      ProductController.indexRestaurant) // the function that will attend requests for that http verb and that path
+
+  app.route('/restaurants/:restaurantId/analytics') // the endpoint path
+    .get( // the http verb that we want to be available at the previous path
+      OrderController.index) // the function that will attend requests for that http verb and that path
 }
 export default loadFileRoutes
